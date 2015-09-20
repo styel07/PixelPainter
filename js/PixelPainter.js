7ad9516c62dde60504bb76c06a39699817b44db0
Brock Lanoza <brocklanoza@gmail.com>
2015-09-19 14:23:32 -1000

    added animation that creates individual hexagons

---
+++ js/PixelPainter.js
@@ -0,0 +1,21 @@
+
+function PixelPainter (width, height, totalW, totalH) {
+
+}
+
+function Canvas () {
+
+  var mainCanvas = document.createElement('canvas');
+  mainCanvas.id = 'mainCanvas';
+
+  var grabTheCanvas = document.getElementById('mainCanvas');
+
+  var combHeight,
+      combRadius,
+      combRectHeight,
+      combRectWidth,
+      combAngle = 0.523598776,
+      sideLength = 36,
+      boardWidth = 10,
+      boardHeight = 10;
+}
